acd7c19ad31ee12c38805df105772b65290e2447
Keerthi Patil <keerthipatil2001@gmail.com>
2026-02-14 16:22:59 -0500

    Implement fetch module for instruction retrieval

---
+++ FETCH.sv
@@ -0,0 +1,19 @@
+module fetch ( input logic [31:0] pc,
+               input logic clk,reset_n,
+               input logic [31:0]imem_data,
+               output logic [31:0] imem_addr,
+               output logic [31:0] instruction,
+               output logic imem_req);
+
+logic req_reg;
+
+always_ff @(posedge clk or negedge reset_n) begin
+if(!reset_n) begin
+req_reg <= 1'b0;end
+else  begin 
+req_reg <= 1'b1;
+end 
+end  
+assign imem_addr = pc;
+assign req_reg = imem_req;
+assign instruction = imem_data;
